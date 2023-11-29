dce2156e843bbfc4a26551e6681da04159423027
Will Binette <wsbinette@icloud.com>
2023-11-29 18:08:20 -0500

    add image to home

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,36 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  final AssetImage headshot = const AssetImage("assets/images/headshot.jpeg");
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Welcome to the Home Page!'),
+    return Column(
+      children: [
+        Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 12, 139, 27),
+                      width: 5,
+                    ),
+                  ),
+                  child: CircleAvatar(
+                    radius: 90,
+                    backgroundImage: headshot,
+                  ),
+                ),
+            ],
+          ),
+      ],
     );
   }
 }
+
