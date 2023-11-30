141c795a060fa2305636f7761d862c78d15a0eea
Will Binette <wsbinette@icloud.com>
2023-11-30 18:04:18 -0500

    add inital cards

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,57 @@
+import 'package:digicard/styles/styles.dart';
 import 'package:digicard/widgets/profile_header.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        ProfileHeader(),
+        const ProfileHeader(),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 5),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  border: Border.all(
+                    color: DigicardStyles.primaryColor,
+                    width: 3,
+                  )
+                ),
+                child: const DefaultTextStyle(
+                  style: TextStyle(
+                    color: DigicardStyles.primaryColor,
+                  ),
+                  child: Row(
+                    // mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      FaIcon(FontAwesomeIcons.linkedin, color: DigicardStyles.primaryColor),
+                      SizedBox(width: 10),
+                      Text(
+                        "LinkedIn",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        )
       ],
     );
   }
