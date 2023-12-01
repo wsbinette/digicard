e10a6e0173550b3a369f1eedd22199334c37fb6e
Will Binette <wsbinette@icloud.com>
2023-12-01 00:49:30 -0500

    update default text style

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:digicard/layouts/default_layout.dart';
 import 'package:digicard/pages/contacts_page.dart';
 import 'package:digicard/pages/home_page.dart';
 import 'package:digicard/pages/qr_page.dart';
+import 'package:digicard/styles/styles.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -21,7 +22,12 @@ class DigicardApp extends StatelessWidget {
           brightness: Brightness.light,
           scaffoldBackgroundColor: CupertinoColors.white,
           textTheme: CupertinoTextThemeData(
-            textStyle: GoogleFonts.montserrat(),
+            textStyle: GoogleFonts.montserrat(
+              color: DigicardStyles.primaryColor,
+              fontSize: 20,
+              fontWeight: FontWeight.w500,
+            ),
+            
           ),
         ),
         home: const DefaultLayout(children: [
