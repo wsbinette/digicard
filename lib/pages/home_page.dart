f8a3a20e9cf58648e5de155539c963a17b35dbf1
Will Binette <wsbinette@icloud.com>
2023-12-12 16:16:33 -0500

    change event name

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,61 +15,57 @@ class HomePage extends StatelessWidget {
       children: [
         const ProfileHeader(),
         Container(
-          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CubeCard(
-                    title: "Text", 
-                    icon: CupertinoIcons.chat_bubble, 
-                    onTap: ()async => MessageLauncher.launchMessage(
-                      "sms:?body=Hi! This is Will Binette. We met at the AI Summit in NYC. I'd love to connect with you! Here's my info below: \n\nWilliam Binette\nFull-Stack Software Engineer | Technical Project Manager\nEmail: wsebinette@gmail.com\Phone: 505-917-7900\nLinkedIn: https://www.linkedin.com/in/wsbinette/\nGithub: https://www.github.com/wsbinette/"
-                    )
-                  ), 
-                  // const Spacer(),
-                  CubeCard(
-                    title: "Email",
-                    icon: CupertinoIcons.envelope,
-                    onTap: ()async => MessageLauncher.launchMessage(
-                      //TODO: add email address here
-                      "mailto:?subject=Will Binette from AI Summit NYC&body=Hi! This is Will Binette. We met at the AI Summit in NYC. I'd love to connect with you! Here's my info below: \n\nWilliam Binette\nFull-Stack Software Engineer | Technical Project Manager\nEmail: wsebinette@gmail.com\Phone: 505-917-7900\nLinkedIn: https://www.linkedin.com/in/wsbinette/\nGithub: https://www.github.com/wsbinette/"
-                    )
-                  )
-                ],
-              ),
-              const SizedBox(height: 10),
-              const ListCard(
-                title: "LinkedIn",
-                icon: FontAwesomeIcons.linkedin,
-                resource: ResourceMeta(name: "LinkedIn", icon: FontAwesomeIcons.linkedin, link: "https://www.linkedin.com/in/wsbinette/"),
-                
-              ),
-              const SizedBox(height: 7),
-              const ListCard(
-                title: "Github",
-                icon: FontAwesomeIcons.github,
-                resource: ResourceMeta(
-                  name: "Github",
-                  icon: FontAwesomeIcons.github,
-                  link: "https://www.github.com/wsbinette/"
+            padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    CubeCard(
+                        title: "Text",
+                        icon: CupertinoIcons.chat_bubble,
+                        onTap: () async => MessageLauncher.launchMessage(
+                            "sms:?body=Hi! This is Will Binette. We met at the NYC Startup Xmas Party. I'd love to connect with you! Here's my info below: \n\nWilliam Binette\nFull-Stack Software Engineer | Technical Project Manager\nEmail: wsebinette@gmail.com\Phone: 505-917-7900\nLinkedIn: https://www.linkedin.com/in/wsbinette/\nGithub: https://www.github.com/wsbinette/")),
+                    // const Spacer(),
+                    CubeCard(
+                        title: "Email",
+                        icon: CupertinoIcons.envelope,
+                        onTap: () async => MessageLauncher.launchMessage(
+                            //TODO: add email address here
+                            "mailto:?subject=Will Binette from NYC Startup Xmas Party&body=Hi! This is Will Binette. We met at the AI Summit in NYC. I'd love to connect with you! Here's my info below: \n\nWilliam Binette\nFull-Stack Software Engineer | Technical Project Manager\nEmail: wsebinette@gmail.com\Phone: 505-917-7900\nLinkedIn: https://www.linkedin.com/in/wsbinette/\nGithub: https://www.github.com/wsbinette/"))
+                  ],
                 ),
-              ),
-              const SizedBox(height: 7),
-              const ListCard(
-                title: "Resume",
-                icon: CupertinoIcons.link,
-                resource: ResourceMeta(
-                    name: "Resume",
-                    icon: CupertinoIcons.cloud_download,
-                    link: "https://wsbinette.github.io/downloads/"),
-              ),
-            ],
-          )
-        )
+                const SizedBox(height: 10),
+                const ListCard(
+                  title: "LinkedIn",
+                  icon: FontAwesomeIcons.linkedin,
+                  resource: ResourceMeta(
+                      name: "LinkedIn",
+                      icon: FontAwesomeIcons.linkedin,
+                      link: "https://www.linkedin.com/in/wsbinette/"),
+                ),
+                const SizedBox(height: 7),
+                const ListCard(
+                  title: "Github",
+                  icon: FontAwesomeIcons.github,
+                  resource: ResourceMeta(
+                      name: "Github",
+                      icon: FontAwesomeIcons.github,
+                      link: "https://www.github.com/wsbinette/"),
+                ),
+                const SizedBox(height: 7),
+                const ListCard(
+                  title: "Resume",
+                  icon: CupertinoIcons.link,
+                  resource: ResourceMeta(
+                      name: "Resume",
+                      icon: CupertinoIcons.cloud_download,
+                      link: "https://wsbinette.github.io/downloads/"),
+                ),
+              ],
+            ))
       ],
     );
   }
--- lib/widgets/list_card.dart
+++ lib/widgets/list_card.dart
@@ -7,29 +7,37 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class ResourceMeta{
+class ResourceMeta {
   final String name;
   final IconData icon;
   final String link;
 
-  const ResourceMeta({required this.name, required this.icon, required this.link});
+  const ResourceMeta(
+      {required this.name, required this.icon, required this.link});
 }
 
-class ListCard extends StatelessWidget{
+class ListCard extends StatelessWidget {
   final String title;
   final IconData icon;
   final ResourceMeta resource;
   final String? customMessage;
 
-  const ListCard({Key? key, required this.title, required this.icon, required this.resource, this.customMessage}) : super(key: key);
+  const ListCard(
+      {Key? key,
+      required this.title,
+      required this.icon,
+      required this.resource,
+      this.customMessage})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     final qrData = context.read<QRCodeData>();
     NavigationProvider nav = Provider.of<NavigationProvider>(context);
 
-    String formattedMessage = "Hi! This is Will Binette from the AI Summit NYC.\nHere's my ${resource.name}: ${resource.link}";
-    if(customMessage != null){
+    String formattedMessage =
+        "Hi! This is Will Binette from the NYC Startup Xmas Party.\nHere's my ${resource.name}: ${resource.link}";
+    if (customMessage != null) {
       formattedMessage = "$customMessage ${resource.link}";
     }
 
@@ -45,10 +53,10 @@ class ListCard extends StatelessWidget{
       // onPressed: () => onTap(),
       child: DefaultTextStyle(
         style: GoogleFonts.montserrat(
-            fontSize: 25,
-            fontWeight: FontWeight.w600,
-            color: DigicardStyles.primaryColor,
-          ),
+          fontSize: 25,
+          fontWeight: FontWeight.w600,
+          color: DigicardStyles.primaryColor,
+        ),
         child: Row(
           children: [
             Column(
@@ -73,17 +81,16 @@ class ListCard extends StatelessWidget{
                             "sms:?body=$formattedMessage")
                       },
                       child: Container(
-                        padding: const EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          color: DigicardStyles.primaryColor,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        width: 50,
-                        height: 50,
-                        child: const Center(
-                          child: Icon(CupertinoIcons.chat_bubble, color: CupertinoColors.white, size: 30)
-                        )
-                      ),
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            color: DigicardStyles.primaryColor,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          width: 50,
+                          height: 50,
+                          child: const Center(
+                              child: Icon(CupertinoIcons.chat_bubble,
+                                  color: CupertinoColors.white, size: 30))),
                     ),
                     const SizedBox(width: 10),
                     GestureDetector(
@@ -100,13 +107,14 @@ class ListCard extends StatelessWidget{
                           ),
                           width: 50,
                           height: 50,
-                          child: const Center(child: Icon(CupertinoIcons.qrcode, color: CupertinoColors.white, size: 30))),
+                          child: const Center(
+                              child: Icon(CupertinoIcons.qrcode,
+                                  color: CupertinoColors.white, size: 30))),
                     )
                   ],
                 ),
               ],
             ),
-
           ],
         ),
       ),
